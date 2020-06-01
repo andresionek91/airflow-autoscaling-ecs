12e793b340789fd827c5ef7f835d5ddc201341a9
Andre Sionek <andresionek91@gmail.com>
2020-06-01 01:19:42 +0100

    final adjustments to templates

--- push-to-ecr.sh
+++ push-to-ecr.sh
@@ -4,9 +4,9 @@ COMMIT_HASH=$(python -c 'from utils import generate_hash;  generate_hash(16)' 2>
 eval $(aws ecr get-login --no-include-email);
 
 echo "Pushing image to ECR: airflow-$ENVIRONMENT:$COMMIT_HASH";
-docker tag airflow-$ENVIRONMENT:latest $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/airflow-$ENVIRONMENT:$COMMIT_HASH;
+docker tag airflow-$ENVIRONMENT $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/airflow-$ENVIRONMENT:$COMMIT_HASH;
 docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/airflow-$ENVIRONMENT:$COMMIT_HASH;
 
 echo "Pushing image to ECR: airflow-$ENVIRONMENT:latest";
-docker tag airflow-$ENVIRONMENT:latest $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/airflow-$ENVIRONMENT:latest;
+docker tag airflow-$ENVIRONMENT $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/airflow-$ENVIRONMENT:latest;
 docker push $AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com/airflow-$ENVIRONMENT:latest;
